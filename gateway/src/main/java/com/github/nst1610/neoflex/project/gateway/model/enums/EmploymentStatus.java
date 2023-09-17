650c029db0431fb5e454431562321c7e434c51fe
Анастасия Семенова <nastyasem65@gmail.com>
2023-09-17 18:27:23 +0300

    fixed

--- gateway/src/main/java/com/github/nst1610/neoflex/project/gateway/model/enums/EmploymentStatus.java
+++
@@ -1,5 +0,0 @@
-package com.github.nst1610.neoflex.project.gateway.model.enums;
-
-public enum EmploymentStatus {
-    UNEMPLOYED, SELF_EMPLOYED, EMPLOYED, BUSINESS_OWNER
-}
--- gateway/src/main/java/com/github/nst1610/neoflex/project/gateway/model/enums/MaritalStatus.java
+++
@@ -1,5 +0,0 @@
-package com.github.nst1610.neoflex.project.gateway.model.enums;
-
-public enum MaritalStatus {
-    MARRIED, DIVORCED, SINGLE, WIDOW_WIDOWER
-}
